34eca4f7555bb94c7bfd4f09b34ec5fd0fe90844
Lukas Karberg <deaderlp@googlemail.com>
2024-03-28 17:07:12 +0100

    added migrations for bucket + policies

---
+++ supabase/migrations/20240328153013_create_user_data_bucket.sql
@@ -0,0 +1,36 @@
+insert into storage.buckets
+  (id, name, public, allowed_mime_types, file_size_limit)
+values
+  ('user_data', 'user_data', true, ARRAY['image/jpeg', 'image/jpg', 'image/png', 'image/webp'], 2097152);
+
+
+create policy "Allow selection for authenticated users."
+on "storage"."objects"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Allow updating for authenticated users."
+on "storage"."objects"
+as permissive
+for update
+to authenticated
+using (true);
+
+
+create policy "Allow inserting for authenticated users."
+on "storage"."objects"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Allow deletion for authenticated users."
+on "storage"."objects"
+as permissive
+for delete
+to authenticated
+using (true);
